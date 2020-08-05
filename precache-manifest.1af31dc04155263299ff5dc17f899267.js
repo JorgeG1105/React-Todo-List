6a4b9e4068235e8a137cffd71d0a0e60481a3757
Jorge Goris <J_goris@live.com>
2020-08-05 01:19:56 -0400

    Updates

---
+++ precache-manifest.1af31dc04155263299ff5dc17f899267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb12ec57f0823383296188c32f509e62",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "c39bf47ded11284814ec",
+    "url": "/React-Todo-List/static/css/main.9c1c0b0e.chunk.css"
+  },
+  {
+    "revision": "a620e08a7e030d79bf2b",
+    "url": "/React-Todo-List/static/js/2.038f041a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-List/static/js/2.038f041a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39bf47ded11284814ec",
+    "url": "/React-Todo-List/static/js/main.78094133.chunk.js"
+  },
+  {
+    "revision": "9dc8ecb02231df6d6a21",
+    "url": "/React-Todo-List/static/js/runtime-main.765d690d.js"
+  }
+]);
